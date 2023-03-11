585474ac5d84013ee5f9843ac3f6f12fbb8a7f97
Max Wilson <wilson.max@gmail.com>
2023-03-11 12:25:00 -0800

    Add some visual logic for start command to fake running a battle

--- Commands.fs
+++ Commands.fs
@@ -2,6 +2,17 @@
 
 open Spectre.Console
 open Spectre.Console.Cli
+open System.Threading.Tasks
+
+type Out =
+    static member WriteLine(txt: string, ?color: Color option) =
+        Panel(txt).RoundedBorder().Collapse().BorderColor(Color.Yellow)
+        |> AnsiConsole.Write
+module Task =
+    let map f t = task { let! v = t in return f v }
+    let ignore t = t |> map ignore
+    let wait (t: _ System.Threading.Tasks.Task) = t.Wait()
+    let runSynchronously (t: _ System.Threading.Tasks.Task) = t.Result
 
 type Settings() =
     inherit CommandSettings()
@@ -13,6 +24,16 @@ type Settings() =
 
 type StartCommand() =
     inherit Command<Settings>()
-    override _.Execute(ctx, settings) =
-        printfn $"Running battles for {settings.exePath} with data {settings.dataPath}"
-        0
+    override _.Execute(ctx, settings) = Task.runSynchronously <| task {
+            do! AnsiConsole.Status().StartAsync("Setting up battle", (fun _ -> task { do! Task.Delay 1000 }))
+            do! AnsiConsole.Progress().StartAsync (fun ctx -> task {
+                Out.WriteLine $"Running battles for {settings.exePath} with data {settings.dataPath}"
+                let t = ctx.AddTask "Running a battle"
+                let t2 = ctx.AddTask "Running a battle"
+                while (t.IsFinished && t2.IsFinished) |> not do
+                    do! Task.Delay 30
+                    t.Increment 1.5
+                    t2.Increment 0.5
+            })
+            return 0
+        }
