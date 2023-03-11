7a86c60692418be9e80908d8ad147ac39cb6d411
Max Wilson <wilson.max@gmail.com>
2023-03-11 15:09:52 -0800

    UX tweak: deliberately overestimate time to reduce the number of times a bad estimate gives a bad UX, hangs at 97%

--- Commands.fs
+++ Commands.fs
@@ -34,14 +34,15 @@ type StartCommand() =
                     do! AnsiConsole.Status().StartAsync($"Setting up battle for {args.Name}", (fun _ -> task { do! Task.Delay 1000 }))
                     Out.WriteLine $"Running battles for {args.FullPath}"
                     do! AnsiConsole.Progress().StartAsync (fun ctx -> task {
-                        Out.WriteLine $"Running battles for {settings.exePath} with data {settings.dataPath}"
+                        Out.WriteLine $"Running battles for {args.Name}"
                         let setup name =
                             let mutable percentage = 0.
                             let progressBar = ctx.AddTask $"Hosting for '{name}'" // by running C:\usr\bin\steam\steamapps\common\Dominions5\win64\dominions5.exe  -c -T -g Warhammer {name}
                             fun expectedTicks -> task {
-                                progressBar.Description <- $"Hosting for '{name}', ETA {expectedTicks} seconds" // show latest ETA based on prior completions
+                                if expectedTicks |> Option.isSome then
+                                    progressBar.Description <- $"Hosting for '{name}', ETA {expectedTicks.Value} seconds" // show latest ETA based on prior completions
                                 let registerProgress tickReached =
-                                    let percentage' = (float tickReached / float expectedTicks) * 100. |> min 97.
+                                    let percentage' = (float tickReached / float (defaultArg expectedTicks 100)) * 100. |> min 97.
                                     if percentage' > percentage then
                                         progressBar.Increment (percentage' - percentage)
                                         percentage <- percentage'
@@ -49,9 +50,10 @@ type StartCommand() =
                                 progressBar.Increment 100 // make sure it's done and not just stuck at 99.9%
                                 return total
                                 }
-                        let lines = [| for ix in 1..10 -> $"name{ix}" |> setup |] // File.ReadAllLinesAsync args.FullPath
+                        let lines = [| for ix in 1..10 -> $"{args.Name}{ix}" |> setup |] // File.ReadAllLinesAsync args.FullPath
                         for line in lines do
-                            let! total = line (if tickCounts.IsEmpty then 100 else tickCounts |> List.map float |> List.average |> int)
+                            // by deliberately overestimating the amount of time it will take to finish we may improve the UX/perceived performance/user satisfaction, by reducing how often it "hangs" at 97% due to a bad estimate.
+                            let! total = line (if tickCounts.IsEmpty then None else tickCounts |> List.map float |> List.average |> ((*) 1.2) |> int |> Some)
                             tickCounts <- total::tickCounts
                     })
                 }
--- Dom5.fs
+++ Dom5.fs
@@ -7,11 +7,11 @@ let fakeHost gameName feedback = task {
     let mutable ticks = 0
     // pretend to run C:\usr\bin\steam\steamapps\common\Dominions5\win64\dominions5.exe  -c -T -g <name>
     let fakeHost = task {
-        do! Task.Delay (r.Next 10 * 1000)
+        do! Task.Delay (500 + r.Next 30 * 100)
     }
     let timer = task {
         while(fakeHost.IsCompleted |> not) do
-            do! Task.Delay 1000
+            do! Task.Delay 100
             ticks <- ticks + 1
             feedback ticks
     }
