64c7751e1c27584233b5a6a1a95d9dfef406c435
Max Wilson <wilson.max@gmail.com>
2023-07-30 11:40:03 -0700

    wip

--- test/TDD.fs
+++ test/TDD.fs
@@ -6,6 +6,9 @@ open Expecto
 type FullPath = FullPath of string
 type OrdersId = OrdersId of Guid
 type GameId = GameId of Guid
+
+let notImpl() = failwith "not implemented"
+
 // for now we're just thinking out loud, trying to model the domain on both sides
 // and make sure we're not overlooking any cases. We already did this on paper,
 // just re-doing it in form suitable for github checkin.
@@ -14,33 +17,81 @@ type GameId = GameId of Guid
 // the "soft" model is an interface to the user's needs and desires (GUI). It is Elmish-based.
 // actors and Elmish both are message-based. They are similar but not identical.
 
-type HardReport =
-    | ReportNew2hCreated
-    | ReportGameDeleted
-type HardInterface =
-    abstract DeleteGame: FullPath -> Async<unit>
-    abstract StartProcessing: gameName: string -> Async<unit>
-// SoftMsg is used to manipulate the softModel and therefore the GUI output.
-// Generally we expect the user to use the View to inform HardControl of changes
-// that the user has requested, and then... generate the appropriate SoftMsg?
-// Not sure.
-type SoftMsg =
-    | ToggleAutoApprove of bool
-    | SetAutoApproveFilter of string
-    | ApproveOrders of OrdersId
-    | DeleteOrders of OrdersId
-    | DeleteGame of GameId
-// softModel needs to be sufficient to render to the screen
-type SoftModel = {
-    autoApprove: bool
-    autoApproveFilter: string option
-    }
+[<AutoOpen>]
+module Hard =
+    type HardReport =
+        | ReportNew2hCreated
+        | ReportGameDeleted
+    type HardInterface =
+        abstract DeleteGame: FullPath -> Async<unit>
+        abstract StartProcessing: gameName: string -> Async<unit>
+    // HardCmd is used to represent user commands, which change the HardModel (as opposed
+    // to the SoftModel, which is updated via SoftCmd).
+    // Generally we expect the user to use the View to inform HardControl of changes
+    // that the user has requested, and then update the SoftModel's mirror via a message.
+    type HardCmd =
+        | ToggleAutoApprove of bool
+        | SetAutoApproveFilter of string
+        | ApproveOrders of OrdersId
+        | DeleteOrders of OrdersId
+        | DeleteGame of GameId
+    type HardMsg =
+        | Report of HardReport
+        | Command of HardCmd
+    type HardModel = {
+        autoApprove: bool
+        autoApproveFilter: string option
+        games: Map<GameId, FullPath>
+        }
+        with static member fresh = { autoApprove = false; autoApproveFilter = None; games = Map.empty }
+    let update (system: HardInterface) (hardModel: HardModel) (hardMsg: HardMsg) =
+        match hardMsg with
+        | Report r -> notImpl()
+        | Command c ->
+            match c with
+            | ToggleAutoApprove b -> { hardModel with autoApprove = b } |> async.Return
+            | SetAutoApproveFilter s -> { hardModel with autoApproveFilter = Some s } |> async.Return
+            | ApproveOrders _ -> notImpl()
+            | DeleteOrders _ -> notImpl()
+            | DeleteGame id ->
+                async {
+                    do! system.DeleteGame(hardModel.games[id]) // hey, is it a problem that this might block the UI from toggling AutoApprove on and off?
+                    // maybe we should send a finishedDeleting message or something instead.
+                    return hardModel
+                    }
+    let create (system: HardInterface) (initialModel: HardModel) =
+        MailboxProcessor.Start (fun inbox ->
+           let rec loop hardModel =
+                async {
+                    let! msg = inbox.Receive()
+                    let! model' = (update system hardModel msg)
+                    return! loop model'
+                }
+           loop initialModel)
+
+[<AutoOpen>]
+module Soft =
+    // Note that SoftMsg is emitted only to update the SoftModel, not the HardModel,
+    // so most user commands are HardCmds instead which emit SoftMsg at the end of the process.
+    type SoftMsg =
+        | Mirror of HardModel
+    type Page =
+        | Home
+        | OrdersView
+        | QueueView
+        | ResultsView
+    type SoftCmd =
+        | NavigateTo of Page
+    // softModel needs to be sufficient to render to the screen, can have extra data like focus
+    type SoftModel = {
+        mirror: HardModel
+        currentPage: Page
+        }
 
 [<Tests>]
 let tests = testList "TDD" [
     testCase "placeholder" <| fun _ ->
+        let hardInterface: HardInterface = notImpl()
+        let softModel = SoftModel.fresh
         Expect.isTrue true "placeholder"
-    test "placeholder2" {
-        Expect.equal (1+1) 2 "1+1 should equal 2"
-        }
     ]
