53ab52788a10a5b935e770027b0186099917c173
Max Wilson <wilson.max@gmail.com>
2023-07-30 18:06:53 -0700

    Hmmm, maybe the external hardInterface should itself be agent-based (in order not to block the UI, as would happen if we used a do! to await the result)

--- Scratch.fsx
+++ Scratch.fsx
@@ -20,14 +20,24 @@ let counter1 =
                     return! loop(n+msg) }
         loop 0)
 type Msg =
+    | Pause of AsyncReplyChannel<unit> * int
     | Increment of AsyncReplyChannel<int> * int
     | Query of AsyncReplyChannel<int>
+let waitAgent (replyChannel: AsyncReplyChannel<unit>) (ms: int) =
+    MailboxProcessor.Start (fun inbox ->
+        async {
+            do! Async.Sleep ms
+            replyChannel.Reply()
+            })
 let counter2 =
     MailboxProcessor.Start (fun inbox ->
        let rec loop n =
             async {
                 let! msg = inbox.Receive()
                 match msg with
+                | Pause(replyChannel, ms) ->
+                    (waitAgent replyChannel ms) |> ignore
+                    return! loop n
                 | Increment(replyChannel, i) ->
                     let n = n + i
                     if n > 100 then do! (System.InvalidOperationException() |> raise)
@@ -39,6 +49,22 @@ let counter2 =
                     return! loop(n)
             }
        loop 0)
+counter2.PostAndAsyncReply(fun replyChannel -> Increment(replyChannel, 2))
+async {
+    let! x = counter2.PostAndAsyncReply(fun replyChannel -> Increment(replyChannel, 2))
+    printfn "x = %d" x
+    let task = counter2.PostAndAsyncReply(fun replyChannel -> Pause (replyChannel, 2000))
+    printfn "waiting..."
+    let! x = counter2.PostAndAsyncReply(fun replyChannel -> Increment(replyChannel, 2))
+    printfn "x = %d" x
+    let! x = counter2.PostAndAsyncReply(fun replyChannel -> Increment(replyChannel, 2))
+    printfn "x = %d" x
+    let! x = counter2.PostAndAsyncReply(fun replyChannel -> Increment(replyChannel, 2))
+    printfn "x = %d" x
+    do! task
+    let! x = counter2.PostAndAsyncReply(fun replyChannel -> Increment(replyChannel, 2))
+    printfn "x = %d" x
+    } |> Async.RunSynchronously
 counter2.PostAndReply(fun replyChannel -> Increment(replyChannel, 2))
 counter2.PostAndReply(fun replyChannel -> Query(replyChannel))
 counter2.TryPostAndReply(timeout= 1000, buildMessage=fun replyChannel -> Increment(replyChannel, 10))
