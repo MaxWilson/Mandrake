88e8add62ecf728e8917ba4e277be6982822b054
Max Wilson <wilson.max@gmail.com>
2023-12-22 12:49:11 -0800

    Remove superfluous devLog

--- Dom5.fs
+++ Dom5.fs
@@ -80,8 +80,8 @@ let setupNewWatcher (savedGamesDirectory: DirectoryPath) (onNew, onUpdated) =
         |> Array.filter (not << ignoreThisFile)
     files |> Array.iter onNew
     let watcher = new FileSystemWatcher (System.IO.Path.GetFullPath savedGamesDirectory)
-    watcher.Changed.Add (fun args -> devLogM "Changed" args; onUpdated args.FullPath)
-    watcher.Created.Add (fun args -> devLogM "Created" args; onNew args.FullPath)
+    watcher.Changed.Add (onUpdated << _.FullPath)
+    watcher.Created.Add (onNew << _.FullPath)
     watcher.IncludeSubdirectories <- true
     watcher.EnableRaisingEvents <- true
     watcherHandle <- Some watcher // keep a reference to the watcher so it doesn't get garbage collected
